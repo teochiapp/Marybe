373ede182bca3a21f2e7c57f1914ae28df237c7a
Teo <teochiapps@gmail.com>
2026-06-22 02:51:18 -0300

    changes url

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
+  url: env('PUBLIC_URL', 'https://admin.marybe.surcodes.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
