708f0ddf6e084d5e4df9940a47c823671fdeea5e
Teo <teochiapps@gmail.com>
2026-06-26 20:24:01 -0300

    asd

--- Backend/config/plugins.js
+++ Backend/config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      providers: {
+        google: {
+          enabled: true,
+          icon: 'google',
+          key: env('GOOGLE_CLIENT_ID', ''),
+          secret: env('GOOGLE_CLIENT_SECRET', ''),
+          callback: `${env('PUBLIC_URL', 'http://localhost:1337')}/api/connect/google/callback`,
+          scope: ['email', 'profile'],
+        },
+      },
+    },
+  },
+});
